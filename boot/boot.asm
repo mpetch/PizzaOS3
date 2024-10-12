1ca4b6b95f33cbe4a508b58e2a39e0aec3d68115
omsurase <omsurase2129@gmail.com>
2024-10-13 01:17:38 +0530

    FAT 16 implemented. file related functions remaining.

--- boot/boot.asm
+++ boot/boot.asm
@@ -4,17 +4,36 @@ BITS 16
 CODE_SEG equ gdt_code - gdt_start
 DATA_SEG equ gdt_data - gdt_start
 
-_start:
-    jmp short start
-    nop
+jmp short start
+nop
 
-times 33 db 0 ; Dummy bios parameter block
+; FAT16 BIOS Parameter Block
+OEMIdentifier           db 'PIZZAOS '
+BytesPerSector          dw 0x200
+SectorsPerCluster       db 0x80
+ReservedSectors         dw 200
+FATCopies               db 0x02
+RootDirEntries          dw 0x40
+NumSectors              dw 0x00
+MediaType               db 0xF8
+SectorsPerFat           dw 0x100
+SectorsPerTrack         dw 0x20
+NumberOfHeads           dw 0x40
+HiddenSectors           dd 0x00
+SectorsBig              dd 0x773594
+; Extended BPB (Dos 4.0)
+DriveNumber             db 0x80
+WinNTBit                db 0x00
+Signature               db 0x29
+VolumeID                dd 0xD105
+VolumeIDString          db 'PIZZAOS BOO'
+SystemIDString          db 'FAT16   '
 
 start:
     cli             ; Disable interrupts while setting up
     xor ax, ax
-    mov ds, ax      ; Set Data Segment to 0x7C0
-    mov es, ax      ; Set Extra Segment to 0x7C0
+    mov ds, ax      ; Set Data Segment to 0
+    mov es, ax      ; Set Extra Segment to 0
     mov ss, ax      ; Set Stack Segment to 0
     mov sp, 0x7C00  ; Set Stack Pointer to 0x7C00 (standard boot sector load address)
     sti             ; Re-enable interrupts
@@ -162,4 +181,3 @@ ata_lba_read:
 ; Pad the boot sector to 510 bytes and add boot signature
 times 510 - ($ - $$) db 0
 dw 0xAA55
-
