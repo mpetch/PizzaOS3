5fc909e8aa8f2a3e880f2ec37bcbc3f030463d32
omsurase <omsurase2129@gmail.com>
2024-10-21 15:26:37 +0530

    task adnd process core implementation

--- kernel/memory/paging/paging.h
+++ kernel/memory/paging/paging.h
@@ -24,5 +24,10 @@ struct paging_4gb_chunk* paging_new_4gb(uint8_t flags);
 void paging_switch(uint32_t* directory);
 void enable_paging();
 uint32_t* paging_4gb_chunk_get_directory(struct paging_4gb_chunk* chunk);
+void paging_free_4gb(struct paging_4gb_chunk* chunk);
+int paging_map_to(uint32_t *directory, void *virt, void *phys, void *phys_end, int flags);
+int paging_map_range(uint32_t* directory, void* virt, void* phys, int count, int flags);
+int paging_map(uint32_t* directory, void* virt, void* phys, int flags);
+void* paging_align_address(void* ptr);
 
 #endif
